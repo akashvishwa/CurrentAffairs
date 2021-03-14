c7e5503c00e934ec583247bcafa58f58170c4910
akashvishwa <54744210+akashvishwa@users.noreply.github.com>
2021-03-14 15:48:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("logo").style.height="100px";
+    document.getElementById("moto").style.fontSize = "25px";
+  } else {
+    document.getElementById("logo").style.height="200px";
+    document.getElementById("moto").style.fontSize = "35px";
+  }
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
